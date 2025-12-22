305ab7e3c71c671cbb26bb11ea42f4397d7ecade
LateKnight <131494166+LateKnight9@users.noreply.github.com>
2025-12-22 10:37:49 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,6 +2,5 @@
 from app.api import app
 
 if __name__ == '__main__':
-    # You would typically use a tool like 'gunicorn' for production
     # Set debug=True for development
     app.run(debug=True)
